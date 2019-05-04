b473b8f48391809bc5f433024847db5c78dc8446
ewn2 <erwin.uppal@gmail.com>
2019-05-04 15:16:06 -0700

    Created read in function

---
+++ mem.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <dirent.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+
+struct proc {
+    int pid;
+    char action;
+    int page;
+};
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    char line[200];
+    int i = 0, all = 1, current;
+    FILE *memList;
+    struct proc input[100];
+    time_t theTime;
+    struct tm * timeinfo;
+    time (&theTime);
+    timeinfo = localtime(&theTime);
+    time_t start = theTime;
+    memList = fopen ("memory.dat", "r");
+    if (memList == NULL) {
+        printf("\nUnable to either read memory.dat file\n");
+        exit (1);
+    }
+    while(fgets(line, sizeof(line), memList) != NULL && i < all) {
+        current = sscanf(line, "%i %c %i", &input[i].pid, &input[i].action, &input[i].page);
+        all++;
+        i++;
+    }
+    all--;
+    fclose(memList);
+}
