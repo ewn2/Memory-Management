ff042445f4322ac521393c1e9ba6fc9286e064e3
ewn2 <erwin.uppal@gmail.com>
2019-05-12 23:25:32 -0700

    LRU Completed

--- mem.c
+++ mem.c
@@ -47,6 +47,7 @@ int main() {
     timeinfo = localtime(&theTime);
     time_t start = theTime;
     int inn[200];
+    int in2[200];
     int ro = 0;
     memList = fopen ("memory.dat", "r");
     if (memList == NULL) {
@@ -69,15 +70,14 @@ int main() {
     all--;
     all--;
 
-
     int j = 0, physFIFO[20], physLRU[20], physRAN[20];
-    while (j < 20)
-    {
+    while (j < 20) {
         physFIFO[j] = 0;
         j++;
     }
     memcpy(physLRU, physFIFO, 20);
     memcpy(physRAN, physFIFO, 20);
+    memcpy(in2, inn, 200);
     int virt[50]; //10ˆ15 bytes of virtual address space for process-> 50 bits for virtual address
     /*
     The virtual address that the process generated has 2 components: the virtual page number and the offset within the page.
@@ -87,30 +87,44 @@ int main() {
     int t = 0, n = 0;
     int slow[200];
     int order[200];
-    while (j < 200)
-    {
+    while (j < 200) {
         slow[j] = 0;
         j++;
     }
     j = 0;
     int mo = 0;
     int cro = 0;
+    int r = 0;
+    int jn = 0;
+    int hap = 0;
+    int mok = 0;
+    int rek = 0;
+    int jos = 0;
     while (j <= all) {
-        if (input[j].action == 'A') {//FIFO will be implemented here
-            if (physFIFO[input[j].page] == 0)
-            {
+        if (input[j].action == 'A') {
+            if (physFIFO[input[j].page] == 0) {
                 physFIFO[input[j].page] = input[j].pid;
+                physLRU[input[j].page] = input[j].pid;
             }
             else {
                 while(t < 20 && n == 0) {
                     if (physFIFO[t] == 0) {
                         physFIFO[t] = input[j].pid;
+                        physLRU[t] = input[j].pid;
                         n = 1;
                     }
                     t++;
                 }
                 while(n == 0) {
                     physFIFO[inn[cro]] = input[j].pid;
+                    while (jos < all) {
+                        if (in2[cro] != 0) {
+                        physLRU[in2[cro]] = input[j].pid;
+                        }
+                        jos++;
+                    }
+                    jos = 0;
+                    rek = 0;
                     cro++;
                     n = 1;
                 }
@@ -119,13 +133,36 @@ int main() {
             }
         }
         if (input[j].action == 'R' || input[j].action == 'W' || input[j].action == 'F') {
+            while (jn < all && hap == 0) {
+                if (in2[jn] == input[j].pid) {
+                    in2[jn] = 0;
+                    hap = 1;
+                }
+                jn++;
+            }
+            hap = 0;
+            jn = 0;
             if(physFIFO[input[j].page] == input[j].pid) {
                 if (input[j].action == 'F') {
                     physFIFO[input[j].page] = 0;
                 }
             }
-            else {
-                int r = 0;
+            if(physLRU[input[j].page] == input[j].pid) {
+                if (input[j].action == 'F') {
+                    physLRU[input[j].page] = 0;
+                }
+            }
+            if(physLRU[input[j].page] != input[j].pid) {
+                r = 0;
+                while (r < 20) {
+                    if (physLRU[r] == input[j].pid) {
+                        physLRU[r] = 0;
+                    }
+                    r++;
+                }
+            }
+            if(physFIFO[input[j].page] != input[j].pid) {
+                r = 0;
                 while (r < 20) {
                     if (physFIFO[r] == input[j].pid) {
                         physFIFO[r] = 0;
@@ -140,14 +177,14 @@ int main() {
                 if (physFIFO[r] == input[j].pid) {
                     physFIFO[r] = 0;
                 }
+                if (physLRU[r] == input[j].pid) {
+                    physLRU[r] = 0;
+                }
                 r++;
             }
         }
         j++;
     }
-
-
-
 
     printf("\nFIFO Policy\n");
     j = 0;
@@ -162,7 +199,7 @@ int main() {
     int done[all];
     int good = 1;
     int tr = 0;
-    while (j <= all) {//Must adjust later so it does not repeat unecessarily
+    while (j <= all) {
         while (tr <= all) {
             if (input[j].pid == done[tr]) {
                 good = 0;
@@ -185,5 +222,48 @@ int main() {
         p = 0;
         j++;
     }
+    printf("\nEND FIFO\n");
+    printf("\nLRU Policy\n");
+    j = 0;
+    while (j < 20) {//Added to check stored memory.dat values
+        printf("\nPhysical Memory Location %i: %i", j, physLRU[j]);
+        j++;
+    }
+    j = 0;
+    p = 0;
+    printf("\n");
+    mar = 0;
+    while(j < all) {
+        done[j] = 0;
+        j++;
+    }
+    j = 0;
+    good = 1;
+    tr = 0;
+    while (j <= all) {
+        while (tr <= all) {
+            if (input[j].pid == done[tr]) {
+                good = 0;
+            }
+            tr++;
+        }
+        tr = 0;
+        if(good == 1) {
+            printf("\nProcess %i exists in Physical Location(s): ", input[j].pid);
+            done[mar] = input[j].pid;
+            mar++;
+            while (p < 20) {
+                if (physLRU[p] == input[j].pid){
+                    printf("%i ", p);
+                }
+                p++;
+            }
+        }
+        good = 1;
+        p = 0;
+        j++;
+    }
+    printf("\nEND LRU\n");
+    printf("\nRANDOM Policy\n");
     fclose(memList);
 }
